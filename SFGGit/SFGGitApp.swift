fa6df53dac7b8235c6d4d1c766eda36923f26e73
Roman Volovelskyi <roman.volovelskyi@shutterfly.com>
2025-11-18 10:26:55 +0200

    Initial commit

---
+++ SFGGit/SFGGitApp.swift
@@ -0,0 +1,17 @@
+//
+//  SFGGitApp.swift
+//  SFGGit
+//
+//  Created by Roman Volovelskyi on 18/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct SFGGitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
